82fc44ec94f92bcc0e702d9e1a61ef5082e49aef
2DPrototype <miaccess077dev@gmail.com>
2025-02-28 18:20:08 +0600

    added new pkgs

--- build_linux_amd64.bat
+++ build_linux_amd64.bat
@@ -2,5 +2,5 @@ mkdir build
 mkdir "build/linux_amd64"
 set GOARCH=amd64
 set GOOS=linux
-go build -o build/linux_amd64/tender -ldflags "-s -w" cli/main.go
+go build -o build/linux_amd64/tender -ldflags "-s -w" cli/tender/main.go
 pause
--- build_windows_386.bat
+++ build_windows_386.bat
@@ -2,5 +2,5 @@ mkdir build
 mkdir "build/windows_386"
 set GOARCH=386
 set GOOS=windows
-go build -o build/windows_386/tender.exe  -ldflags "-s -w" cli/main.go
+go build -o build/windows_386/tender.exe  -ldflags "-s -w" cli/tender/main.go
 pause
