575cba5cf3bae5e1d3133fb02ed52486e8da2d4f
2DPrototype <miaccess077dev@gmail.com>
2025-07-31 17:18:46 +0600

    added colors

--- go.mod
+++ go.mod
@@ -11,13 +11,25 @@ require (
 	golang.org/x/crypto v0.22.0
 	golang.org/x/image v0.15.0
 	golang.org/x/mobile v0.0.0-20240401224555-755f4ef71bbd
-	golang.org/x/sys v0.19.0
+	golang.org/x/sys v0.30.0
 )
 
 require (
 	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802 // indirect
 	github.com/BurntSushi/xgbutil v0.0.0-20160919175755-f7c97cef3b4e // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/lipgloss v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/ebitengine/purego v0.6.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/oakmound/w32 v2.0.0+incompatible // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.21.0 // indirect
 )
--- stdlib/colors.go
+++ stdlib/colors.go
@@ -3,6 +3,7 @@ package stdlib
 import (
 	"github.com/2dprototype/tender/v/colorable"
 	"github.com/2dprototype/tender"
+	"github.com/charmbracelet/lipgloss"
 )
 
 var colorsModule = map[string]tender.Object{
@@ -20,6 +21,103 @@ var colorsModule = map[string]tender.Object{
 				return nil, tender.ErrWrongNumArguments
 			}
 			return &IOWriter{Value: colorable.NewColorableStderr()}, nil
+		},
+	},
+	"style": &tender.UserFunction{
+		Value: func(args ...tender.Object) (tender.Object, error) {
+			if len(args) < 1 {
+				return nil, tender.ErrWrongNumArguments
+			}
+			
+			text, ok := tender.ToString(args[0])
+			if !ok {
+				return nil, tender.ErrInvalidArgumentType{
+					Name:     "text",
+					Expected: "string",
+					Found:    args[0].TypeName(),
+				}
+			}
+			
+			style := lipgloss.NewStyle()
+			for _, arg := range args[1:] {
+				switch prop := arg.(type) {
+				case *tender.Map:
+					for k, v := range prop.Value {
+						switch k {
+						case "color":
+							if s, ok := v.(*tender.String); ok {
+								style = style.Foreground(lipgloss.Color(s.Value))
+							}
+						case "background":
+							if s, ok := v.(*tender.String); ok {
+								style = style.Background(lipgloss.Color(s.Value))
+							}
+						case "bold":
+							if b, ok := tender.ToBool(v); ok {
+								style = style.Bold(b)
+							}
+						case "italic":
+							if b, ok := tender.ToBool(v); ok {
+								style = style.Italic(b)
+							}
+						case "underline":
+							if b, ok := tender.ToBool(v); ok {
+								style = style.Underline(b)
+							}
+						case "strikethrough":
+							if b, ok := tender.ToBool(v); ok {
+								style = style.Strikethrough(b)
+							}
+						case "width":
+							if i, ok := tender.ToInt(v); ok {
+								style = style.Width(i)
+							}
+						case "height":
+							if i, ok := tender.ToInt(v); ok {
+								style = style.Height(i)
+							}
+						case "align":
+							if s, ok := v.(*tender.String); ok {
+								switch s.Value {
+								case "left":
+									style = style.Align(lipgloss.Left)
+								case "center":
+									style = style.Align(lipgloss.Center)
+								case "right":
+									style = style.Align(lipgloss.Right)
+								}
+							}
+						case "border":
+							if s, ok := v.(*tender.String); ok {
+								switch s.Value {
+								case "normal":
+									style = style.BorderStyle(lipgloss.NormalBorder())
+								case "rounded":
+									style = style.BorderStyle(lipgloss.RoundedBorder())
+								case "thick":
+									style = style.BorderStyle(lipgloss.ThickBorder())
+								case "double":
+									style = style.BorderStyle(lipgloss.DoubleBorder())
+								}
+							}
+						case "border_top":
+							if b, ok := tender.ToBool(v); ok {
+								style = style.BorderTop(b)
+							}
+						case "margin":
+							if i, ok := tender.ToInt(v); ok {
+								style = style.Margin(i)
+							}
+						case "padding":
+							if i, ok := tender.ToInt(v); ok {
+								style = style.Padding(i)
+							}
+						}
+					}
+				}
+			}
+			
+			return &tender.String{Value: style.Render(text)}, nil
 		},
 	},
 }
